dee794d017e3a8705d9963857c11a7b3b146a4bf
Alvinripper <32291465+Alvinripper@users.noreply.github.com>
2017-09-26 00:45:52 -0700

    helloworld.cpp
    this is a simple hello world c++ program

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+/*this is a hello world program in c++ as am learning
+* how to use git and github */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+cout<< "hello world \n";
+
+return 0;
+}
